997c8e8eb7b89f6bc537ac046623fa7f4f2e049b
argebynogame <arge@bynogame.com>
2017-10-18 16:01:05 +0300

    ADD .js deleted

--- components/quick-form/components/quick-form-buttons/quick-form-button.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var QuickFormButton = (function () {
-    function QuickFormButton() {
-    }
-    return QuickFormButton;
-}());
-exports.QuickFormButton = QuickFormButton;
