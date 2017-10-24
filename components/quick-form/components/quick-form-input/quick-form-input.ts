8d0c6e1fa546c27a3f8b2161ae9e829c0aec7416
argebynogame <arge@bynogame.com>
2017-10-24 12:55:16 +0300

    FIX input component

--- components/quick-form/components/quick-form-input/quick-form-input.ts
+++ components/quick-form/components/quick-form-input/quick-form-input.ts
@@ -2,9 +2,11 @@ export class QuickFormInput {
     public label: string;
     public type: string;
     public regex: string;
-    public required: string;
+    public required: boolean;
     public class: string;
     public value: string;
     public control_class: string;
     public form_control_name: string;
+    public min_character: number;
+    public max_character: number;
 }
