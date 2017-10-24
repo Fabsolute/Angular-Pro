f36644a341dd7b9d222e7a9097786ad39dd8610d
argebynogame <arge@bynogame.com>
2017-10-24 12:54:44 +0300

    FIX input component

--- components/quick-form/components/quick-form-input/quick-form-input.component.ts
+++ components/quick-form/components/quick-form-input/quick-form-input.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ComponentBase} from '../base.component';
+import {ComponentBase} from '../../../base.component';
 import {QuickFormInput} from './quick-form-input';
 import {QuickFormService} from '../../services/quick-form.service';
 import {FormControl, Validators} from '@angular/forms';
@@ -11,8 +11,13 @@ import {FormControl, Validators} from '@angular/forms';
     providers: [{provide: ComponentBase, useExisting: QuickFormInputComponent}]
 })
 export class QuickFormInputComponent extends ComponentBase implements OnInit {
-
-    @Input() protected options: QuickFormInput;
+    @Input() protected form_control_name: string;
+    @Input() protected label: string;
+    @Input() protected required: boolean;
+    @Input() protected min_character: number;
+    @Input() protected max_character: number;
+    @Input() protected class: string;
+    @Input() protected value: string;
 
     constructor(public quick_form_service: QuickFormService) {
         super();
@@ -21,16 +26,36 @@ export class QuickFormInputComponent extends ComponentBase implements OnInit {
     control: FormControl;
 
     ngOnInit() {
-        console.log(this.options);
-        if (this.options) {
-            this.control = new FormControl(this.options.form_control_name,
-                [Validators.required,
-                    Validators.minLength(3),
-                    Validators.maxLength(10)
-                ]);
-            this.quick_form_service.toFormControl(this.control);
 
+        // const options: QuickFormInput = {
+        //     label: this.label,
+        //     required: this.required,
+        //     regex: this.regex,
+        //     type: this.type,
+        //     class: this.class,
+        //     value: this.value,
+        //     control_class: this.control_class,
+        //     form_control_name: this.form_control_name,
+        //     min_character: this.min_character,
+        //     max_character: this.max_character
+        // };
+
+        if (this.required) {
+            this.control = new FormControl(this.form_control_name,
+                [
+                    Validators.required,
+                    Validators.minLength((this.min_character) ? this.min_character : 0),
+                    Validators.maxLength((this.max_character) ? this.max_character : 100)
+                ]);
+        } else {
+            this.control = new FormControl(this.form_control_name,
+                [
+                    Validators.minLength((this.min_character) ? this.min_character : 0),
+                    Validators.maxLength((this.max_character) ? this.max_character : 100)
+                ]);
         }
 
+
+        this.quick_form_service.toFormControl(this.control);
     }
 }
