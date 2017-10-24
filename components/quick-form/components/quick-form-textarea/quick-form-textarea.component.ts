a3064c994ec48e06f4a4c208869f581dbe41c2ae
argebynogame <arge@bynogame.com>
2017-10-24 12:57:09 +0300

    FIX textarea component

--- components/quick-form/components/quick-form-textarea/quick-form-textarea.component.ts
+++ components/quick-form/components/quick-form-textarea/quick-form-textarea.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ComponentBase} from '../base.component';
+import {ComponentBase} from '../../../base.component';
 import {QuickFormTextarea} from './quick-form-textarea';
 import {QuickFormService} from '../../services/quick-form.service';
 
@@ -15,7 +15,15 @@ import {FormControl, Validators} from '@angular/forms';
 export class QuickFormTextareaComponent extends ComponentBase implements OnInit {
 
 
-    @Input() protected options: QuickFormTextarea;
+    @Input() protected label: string;
+    @Input() protected required: boolean;
+    @Input() protected regex: string;
+    @Input() protected min_character: number;
+    @Input() protected max_character: number;
+    @Input() protected class: string;
+    @Input() protected value: string;
+    @Input() protected control_class: string;
+    @Input() protected form_control_name: string;
 
 
     constructor(public quick_form_service: QuickFormService) {
@@ -26,16 +34,24 @@ export class QuickFormTextareaComponent extends ComponentBase implements OnInit 
     control: FormControl;
 
     ngOnInit() {
-        console.log(this.options);
-        if (this.options) {
-            this.control = new FormControl(this.options.form_control_name,
+
+        if (this.required) {
+            this.control = new FormControl(this.form_control_name,
                 [
                     Validators.required,
-                    Validators.minLength(3),
-                    Validators.maxLength(10)
+                    Validators.minLength((this.min_character) ? this.min_character : 0),
+                    Validators.maxLength((this.max_character) ? this.max_character : 500)
                 ]);
-            this.quick_form_service.toFormControl(this.control);
+        } else {
+            this.control = new FormControl(this.form_control_name,
+                [
+                    Validators.minLength((this.min_character) ? this.min_character : 0),
+                    Validators.maxLength((this.max_character) ? this.max_character : 500)
+                ]);
         }
+
+
+        this.quick_form_service.toFormControl(this.control);
 
     }
 }
